e7b464644acb60d8405e6ca34aff8a3756f00bdb
loltisha <alyahadiem@gmail.com>
2019-02-20 16:04:53 +0300

    Updates

---
+++ precache-manifest.9fc0eec3cce94a6cc93d0dc99fbd1769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84baf89f9ba5d6d794b8",
+    "url": "/project_2/static/css/main.6d79da59.chunk.css"
+  },
+  {
+    "revision": "84baf89f9ba5d6d794b8",
+    "url": "/project_2/static/js/main.84baf89f.chunk.js"
+  },
+  {
+    "revision": "84d62990d1ae1918d42a",
+    "url": "/project_2/static/js/runtime~main.84d62990.js"
+  },
+  {
+    "revision": "23ba732b6b463f1ff3e5",
+    "url": "/project_2/static/js/2.23ba732b.chunk.js"
+  },
+  {
+    "revision": "8e756dcb2097cda6bea81440e569db48",
+    "url": "/project_2/index.html"
+  }
+];
